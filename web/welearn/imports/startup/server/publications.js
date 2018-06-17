90828f5836ad3275c168e7413c2b6dcbd921f492
uniquarkD <sainu_u@yahoo.com>
2018-06-17 09:04:11 -0700

    blacklist some test fields

--- web/welearn/imports/startup/server/publications.js
+++ web/welearn/imports/startup/server/publications.js
@@ -5,16 +5,16 @@ import {
   TestRewards,
   Donations,
   RewardTransactions,
-} from "../../api/collections";
+} from "../../api/collections"
 
 Meteor.publish("currencies", function () {
   return Currencies.find({}, { limit: 1 })
 })
 
 Meteor.publish("tests", function (limit) {
-  return Tests.find({}, { sort: { createdAt: -1 }, limit: limit || 10, fields: { quizzesArr: 0 } })
+  return Tests.find({}, { sort: { createdAt: -1 }, limit: limit || 10, fields: { quizzesArr: 0, correctAnswerIndex: 0 } })
 })
 
 Meteor.publish("singleTest", function (testId) {
-  return Tests.find({ _id: testId })
+  return Tests.find({ _id: testId }, { fields: { correctAnswerIndex: 0 } })
 })
